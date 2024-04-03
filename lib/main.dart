08f96d3a583df24d469a972a49d12887b791ffc1
hashemhassen <hashemhassen0@gmail.com>
2024-04-03 21:02:12 +0200

    firebase android setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:apartmy/features/home_view/views/home_view.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const Apartmy());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          apiKey: "AIzaSyDPN-2u1Aov-eRRkMM409bHfJN1fP0Xskw",
+          appId: "1:1:1015665773186:android:00df489d2701142232829b",
+          messagingSenderId: "1015665773186",
+          projectId: "apartmy-1782c",
+        ))
+      : SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(
+    const Apartmy(),
+  );
 }
 
 class Apartmy extends StatelessWidget {
