97cf0c4c6363ea14fa0db6aca6a913c89379a32b
hashemhassen <hashemhassen0@gmail.com>
2024-04-28 23:09:02 +0300

    setting everything up

--- lib/features/adding_block_view/views/adding_block_view.dart
+++ lib/features/adding_block_view/views/adding_block_view.dart
@@ -5,7 +5,8 @@ class AddingBlockView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      appBar: AppBar(),
       body: AddingBlockViewBody(),
     );
   }
--- lib/features/home_view/widgets/home_view_body.dart
+++ lib/features/home_view/widgets/home_view_body.dart
@@ -10,12 +10,24 @@ class HomeViewBody extends StatefulWidget {
 }
 
 class _HomeViewBodyState extends State<HomeViewBody> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('N/V'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton.outlined(
+              iconSize: 40,
+              onPressed: () {
+               // todo: add about dialog to take inputs
+              },
+              icon: Icon(Icons.home),
+            ),
+            SizedBox(height: 8),
+            Text('Create a new block'),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
