9a479de315f0d0f53ee11e7767e71c6aeedf8bd6
hashemhassen <hashemhassen0@gmail.com>
2024-04-30 00:12:01 +0300

    taking block name + floors number for user

--- lib/features/home_view/widgets/home_view_body.dart
+++ lib/features/home_view/widgets/home_view_body.dart
@@ -1,6 +1,4 @@
-import 'package:apartmy/core/utils/routes_names.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
@@ -10,6 +8,26 @@ class HomeViewBody extends StatefulWidget {
 }
 
 class _HomeViewBodyState extends State<HomeViewBody> {
+  late TextEditingController nameController;
+  late TextEditingController floorsController;
+  String? name = '';
+  int? floors = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController();
+    floorsController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    floorsController.dispose();
+    super.dispose();
+  }
+
+//todo: remove displaying of number of floors & name and navigate to adding block
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +35,23 @@ class _HomeViewBodyState extends State<HomeViewBody> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(
+              name??'No name yet',
+              style: TextStyle(fontSize: 40),
+            ),
+            Text(
+              floors.toString(),
+              style: TextStyle(fontSize: 40),
+            ),
             IconButton.outlined(
               iconSize: 40,
-              onPressed: () {
-               // todo: add about dialog to take inputs
+              onPressed: () async {
+                final nameAndFloors = await onDialog(context);
+                if (nameAndFloors == null) return;
+                setState(() {
+                  this.name = nameAndFloors[0];
+                  this.floors = int.parse(nameAndFloors[1]);
+                });
               },
               icon: Icon(Icons.home),
             ),
@@ -29,12 +60,57 @@ class _HomeViewBodyState extends State<HomeViewBody> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.pushNamed(RoutesNames.addingBlock);
-        },
-        child: Icon(Icons.add),
-      ),
     );
+  }
+
+  Future onDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Create a new block'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                keyboardType: TextInputType.name,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: 'Block Name',
+                ),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                  keyboardType: TextInputType.number,
+                  controller: floorsController,
+                  decoration: InputDecoration(
+                    hintText: 'Number of floors',
+                  )),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: submit,
+              child: Text('Create'),
+            ),
+            TextButton(
+              onPressed: cancel,
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void submit() {
+    Navigator.of(context).pop([nameController.text, floorsController.text]);
+    nameController.clear();
+    floorsController.clear();
+  }
+
+  void cancel() {
+    Navigator.of(context).pop();
   }
 }
